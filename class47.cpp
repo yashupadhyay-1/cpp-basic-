972fa0d7b2f1e368b2361e799dc18df8deb6a031
yashupadhyay-1 <yashupadhyay1207@gmail.com>
2025-05-08 18:21:15 +0530

    Add files via upload

--- class47.cpp
+++ class47.cpp
@@ -1,53 +1,72 @@
 #include<iostream>
 using namespace std;
-class stack{
+class TwoStack{
     public:
     int *arr;
-    int top;
+    int top1;
+    int top2;
     int size;
-    
-    //calling a function
-    stack(int size){
-        this->size = size;
+    //Making a new stack
+    TwoStack(int size){
+        this -> size = size;
+        top1 = -1;
+        top2 = size;
         arr = new int[size];
-        top = -1;
     }
-    
-    void push(int n){
-        if(top==size-1){
-            cout<<"Stack Overflow";
-        }
-        else{
-            top++;
-            arr[top] = n;
-        }
-    }
-    
-    void pop(){
-        if(top>=0){
-            top--;
-        }
-        else{
-            cout<<"Stack Underflow";
-        }
-    }
-    int peek(){
-        if(top>=0){
-            return arr[top];
-        }
-        else{
-            cout<<"Stack is empty";
-            return -1;
-        }
-    }
+    //push in the first stack
+     void push1(int element){
+         if(top2-top1==1){
+             cout<<"Stack is full"<<endl;
+             return;
+         }
+         else{
+             top1++;
+             arr[top1] = element;
+         }
+     }
+     void push2(int element){
+         if(top2-top1==1){
+             cout<<"Stack is full"<<endl;
+             return;
+         }
+         else{
+             top2--;
+             arr[top2]=element;
+         }
+     }
+     void pop1(){
+         if(top1==-1){
+             cout<<"Stack is underflow"<<endl;
+         }
+         else{
+             top1--;
+         }
+     }
+     void pop2(){
+         if(top2==size){
+             cout<<"Stack2 is underflow"<<endl;
+         }
+         else{
+             top2++;
+         }
+     }
+     int gettop1(){
+         return arr[top1];
+     }
+     int gettop2(){
+         return arr[top2];
+     }
 };
 int main(){
-    stack st(5);
-    st.push(43);
-    st.push(34);
-    cout<<st.peek();
-    st.pop();
-    cout<<endl<<"After"<<endl<<st.peek();
+    TwoStack st(5);
+    st.push1(3);
+    st.push1(4);
+    st.push1(5);
+    st.push2(7);
+    st.push2(8);
+    st.push2(6);
+    cout<<st.gettop2()<<endl;
+    cout<<st.gettop1();
 }
 
 
